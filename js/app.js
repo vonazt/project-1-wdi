8f15d13733b453cc9accb84817f57baaf91d653c
Richard Tzanov <richardtzanov@gmail.com>
2018-05-19 11:28:00 +0100

    refactored the shit out of the playerOne/Two functions. still need a neat solution for game.showAvailableSquares, but each movement based on flag is down to single function call based on playerOneTurn flag

--- js/app.js
+++ js/app.js
@@ -39,60 +39,48 @@ game.drawBattlefield = function drawBattlefield() {
   });
 };
 
+game.characterMovement = function characterMovement(player, e) {
+  const $characterOnBoard = $(player);
+  const characterId = $characterOnBoard.attr('id');
+  const upSquare = $(`#${parseInt(characterId[0])-1}-${parseInt(characterId[2])}`);
+  const downSquare = $(`#${parseInt(characterId[0])+1}-${parseInt(characterId[2])}`);
+  const leftSquare = $(`#${parseInt(characterId[0])}-${parseInt(characterId[2]) -1}`);
+  const rightSquare = $(`#${parseInt(characterId[0])}-${parseInt(characterId[2]) + 1}`);
+
+  if (e.which === 38) {
+    game.makeMove(upSquare, $characterOnBoard);
+  } else if (e.which === 40) {
+    game.makeMove(downSquare, $characterOnBoard);
+  } else if (e.which === 39) {
+    game.makeMove(rightSquare, $characterOnBoard);
+  } else if (e.which === 37) {
+    game.makeMove(leftSquare, $characterOnBoard);
+  }
+};
 
 game.moveCharacter = function moveCharacter() {
   $(document).on('keydown', function(e) {
     if (game.playerOneTurn) {
-      const $characterOnBoard = $('.playerOne');
-      const characterId = $characterOnBoard.attr('id');
-      const upSquare = $(`#${parseInt(characterId[0])-1}-${parseInt(characterId[2])}`);
-      const downSquare = $(`#${parseInt(characterId[0])+1}-${parseInt(characterId[2])}`);
-      const leftSquare = $(`#${parseInt(characterId[0])}-${parseInt(characterId[2]) -1}`);
-      const rightSquare = $(`#${parseInt(characterId[0])}-${parseInt(characterId[2]) + 1}`);
-
-      if (e.which === 38) {
-        game.makeMove(upSquare, $characterOnBoard);
-      } else if (e.which === 40) {
-        game.makeMove(downSquare, $characterOnBoard);
-      } else if (e.which === 39) {
-        game.makeMove(rightSquare, $characterOnBoard);
-      } else if (e.which === 37) {
-        game.makeMove(leftSquare, $characterOnBoard);
-      }
+      game.characterMovement('.playerOne', e);
     } else {
-      const $characterOnBoard = $('.playerTwo');
-      const characterId = $characterOnBoard.attr('id');
-      const upSquare = $(`#${parseInt(characterId[0])-1}-${parseInt(characterId[2])}`);
-      const downSquare = $(`#${parseInt(characterId[0])+1}-${parseInt(characterId[2])}`);
-      const leftSquare = $(`#${parseInt(characterId[0])}-${parseInt(characterId[2]) -1}`);
-      const rightSquare = $(`#${parseInt(characterId[0])}-${parseInt(characterId[2]) + 1}`);
-
-      if (e.which === 38) {
-        game.makeMove(upSquare, $characterOnBoard);
-      } else if (e.which === 40) {
-        game.makeMove(downSquare, $characterOnBoard);
-      } else if (e.which === 39) {
-        game.makeMove(rightSquare, $characterOnBoard);
-      } else if (e.which === 37) {
-        game.makeMove(leftSquare, $characterOnBoard);
-      }
+      game.characterMovement('.playerTwo', e);
     }
   });
 };
 
+game.moveCells = function moveCells(player, direction, characterOnBoard) {
+  if (direction.attr('class') === 'battle-cell available') {
+    direction.removeClass('battle-cell available').addClass(player);
+    characterOnBoard.removeClass(player).addClass('battle-cell available');
+  }
+};
+
 game.makeMove = function makeMove(direction, characterOnBoard) {
   if (this.playerOneTurn) {
-    if (direction.attr('class') === 'battle-cell available') {
-      direction.removeClass('battle-cell available').addClass('playerOne');
-      characterOnBoard.removeClass('playerOne').addClass('battle-cell available');
-    }
+    this.moveCells('playerOne', direction, characterOnBoard);
   } else {
-    if (direction.attr('class') === 'battle-cell available') {
-      direction.removeClass('battle-cell available').addClass('playerTwo');
-      characterOnBoard.removeClass('playerTwo').addClass('battle-cell available');
-    }
+    this.moveCells('playerTwo', direction, characterOnBoard);
   }
-
 };
 
 const character = {};
@@ -102,78 +90,48 @@ character.moveStats = {
   y: 2
 };
 
-game.checkMoveDistance = function checkMoveDistance() {
-  if (this.playerOneTurn) {
-    const $characterStartPoint = $('.playerOne').attr('id');
-    const characterXStartpoint = parseInt($characterStartPoint[0]);
-    const characterYStartPoint = parseInt($characterStartPoint[2]);
+game.showAvailableSquares = function showAvailableSquares(player) {
+  const $characterStartPoint = $(player).attr('id');
+  const characterXStartpoint = parseInt($characterStartPoint[0]);
+  const characterYStartPoint = parseInt($characterStartPoint[2]);
 
-    const moveArray = [];
-    let i = 0;
-    const availableLength = character.moveStats.y;
-    const xDistance = i + characterXStartpoint;
-    const yDistance = i + characterYStartPoint;
+  const moveArray = [];
+  let i = 0;
+  const availableLength = character.moveStats.y;
+  const xDistance = i + characterXStartpoint;
+  const yDistance = i + characterYStartPoint;
 
-    for (i; i <= availableLength; i++) {
-      moveArray.push((i + xDistance) + '-' + (i + yDistance));
-      moveArray.push((i + xDistance) + '-' + (yDistance - i));
-      moveArray.push((xDistance - i) + '-' + (yDistance - i));
-      moveArray.push((xDistance - i) + '-' + (i + yDistance));
-      moveArray.push(xDistance + '-' + (i + yDistance));
-      moveArray.push(xDistance + '-' + (yDistance - i));
-      moveArray.push((xDistance + i) + '-' + (yDistance));
-      moveArray.push((xDistance - i) + '-' + (yDistance));
-      moveArray.push((xDistance + i) + '-' + (yDistance - i+1));
-      moveArray.push((xDistance + i) + '-' + (yDistance + i-1));
-      moveArray.push((xDistance - i) + '-' + (yDistance - i + 1));
-      moveArray.push((xDistance - i) + '-' + (yDistance + i-1));
-    }
-
-    const $gridIds = $('.battle-cell');
-    $gridIds.each(function() {
-      const id = this.id;
-      if (moveArray.includes(id)) {
-        $(this).addClass('available');
-      }
-    });
-  } else {
-    const $characterStartPoint = $('.playerTwo').attr('id');
-    const characterXStartpoint = parseInt($characterStartPoint[0]);
-    const characterYStartPoint = parseInt($characterStartPoint[2]);
-
-    const moveArray = [];
-    let i = 0;
-    const availableLength = character.moveStats.y;
-    const xDistance = i + characterXStartpoint;
-    const yDistance = i + characterYStartPoint;
-
-    for (i; i <= availableLength; i++) {
-      moveArray.push((i + xDistance) + '-' + (i + yDistance));
-      moveArray.push((i + xDistance) + '-' + (yDistance - i));
-      moveArray.push((xDistance - i) + '-' + (yDistance - i));
-      moveArray.push((xDistance - i) + '-' + (i + yDistance));
-      moveArray.push(xDistance + '-' + (i + yDistance));
-      moveArray.push(xDistance + '-' + (yDistance - i));
-      moveArray.push((xDistance + i) + '-' + (yDistance));
-      moveArray.push((xDistance - i) + '-' + (yDistance));
-      moveArray.push((xDistance + i) + '-' + (yDistance - i+1));
-      moveArray.push((xDistance + i) + '-' + (yDistance + i-1));
-      moveArray.push((xDistance - i) + '-' + (yDistance - i + 1));
-      moveArray.push((xDistance - i) + '-' + (yDistance + i-1));
-    }
-
-    const $gridIds = $('.battle-cell');
-    $gridIds.each(function() {
-      const id = this.id;
-      if (moveArray.includes(id)) {
-        $(this).addClass('available');
-      }
-    });
+  for (i; i <= availableLength; i++) {
+    moveArray.push((i + xDistance) + '-' + (i + yDistance));
+    moveArray.push((i + xDistance) + '-' + (yDistance - i));
+    moveArray.push((xDistance - i) + '-' + (yDistance - i));
+    moveArray.push((xDistance - i) + '-' + (i + yDistance));
+    moveArray.push(xDistance + '-' + (i + yDistance));
+    moveArray.push(xDistance + '-' + (yDistance - i));
+    moveArray.push((xDistance + i) + '-' + (yDistance));
+    moveArray.push((xDistance - i) + '-' + (yDistance));
+    moveArray.push((xDistance + i) + '-' + (yDistance - i+1));
+    moveArray.push((xDistance + i) + '-' + (yDistance + i-1));
+    moveArray.push((xDistance - i) + '-' + (yDistance - i + 1));
+    moveArray.push((xDistance - i) + '-' + (yDistance + i-1));
   }
 
+  const $gridIds = $('.battle-cell');
+  $gridIds.each(function() {
+    const id = this.id;
+    if (moveArray.includes(id)) {
+      $(this).addClass('available');
+    }
+  });
 };
 
-
+game.checkMoveDistance = function checkMoveDistance() {
+  if (this.playerOneTurn) {
+    this.showAvailableSquares('.playerOne');
+  } else {
+    this.showAvailableSquares('.playerTwo');
+  }
+};
 
 $(() => {
   game.drawBattlefield();
